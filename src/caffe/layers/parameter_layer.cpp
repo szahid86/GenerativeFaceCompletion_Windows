781beb78d8e0e6bf54652033e4d70e42d741d599
Zahid Ali <zahid86.nedian@yahoo.com>
2018-12-16 22:46:09 -0800

    Add files via upload

---
+++ src/caffe/layers/parameter_layer.cpp
@@ -0,0 +1,9 @@
+#include "caffe/layers/parameter_layer.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+INSTANTIATE_CLASS(ParameterLayer);
+REGISTER_LAYER_CLASS(Parameter);
+
+}  // namespace caffe
